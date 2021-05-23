f1a753c0a6ccac38600e1038b2599165352d5302
nazeer as <nazeer.saw@gmail.com>
2021-05-23 03:07:15 -0700

    Update SocialNetworkApplication.java

--- src/main/java/com/hawaya/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/hawaya/socialnetwork/SocialNetworkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SocialNetworkApplication {
 //test change local
 //test c1
+	//test r2
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
 	}
