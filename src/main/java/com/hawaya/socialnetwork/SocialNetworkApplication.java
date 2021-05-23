d65ff1f779f37bd5770936147bec2851870ae1e9
nazeersw <nazeer.saw@gmail.com>
2021-05-23 05:44:51 -0700

    xx

--- src/main/java/com/hawaya/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/hawaya/socialnetwork/SocialNetworkApplication.java
@@ -11,17 +11,5 @@ public class SocialNetworkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
 	}
-<<<<<<< HEAD
-
-=======
-//eclips remote 2	
-//eclips local
-//eclips remote	
-//eclips
-	//eclips2
-//mn1
-//mn2	
-//cr2
->>>>>>> 7db231bd2f43a2298b31bf39310392155dd3c5f7
 
 }
