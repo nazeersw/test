75d518eaa43088f8efafab17c159759c6f8a26fd
nazeer <nazer5479@hotmail.com>
2021-05-23 02:24:38 -0700

    chane L2

--- src/main/java/com/hawaya/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/hawaya/socialnetwork/SocialNetworkApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SocialNetworkApplication {
 //test change local
 	public static void main(String[] args) {
-		SpringApplication.run(SocialNetworkApplication.class, args);
+		//SpringApplication.run(SocialNetworkApplication.class, args);
 	}
 
 }
